650d7ba50eb6ff850329725f80431299a97e5a94
Vladyslav Hurzhyi <soulline0@gmail.com>
2022-11-26 14:34:12 +0100

    blogCard

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,4 @@
+import { formatDistanceToNow } from 'date-fns';
 import {
   Card,
   CardBody,
@@ -11,9 +12,38 @@ import {
   UserBox,
   UserInfo,
   UserName,
-  Date,
+  Date as Time,
 } from './BlogCard.styled';
 
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+export const BlogCard = ({
+  poster,
+  tag,
+  title,
+  description,
+  userName,
+  avatar,
+  postedAt,
+}) => {
+  const time = formatDistanceToNow(new Date(postedAt));
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster src={poster} alt={tag} />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt={userName} />
+          <UserInfo>
+            <UserName>{userName}</UserName>
+            <Time>{time}</Time>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
 };
